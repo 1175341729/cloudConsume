7d75dca3cfedad611d52e803366a803383cc3f7a
1175341729@qq.com <5238121abc>
2017-12-04 16:06:32 +0800

    初始化

---
+++ src/main/java/com/eureka/consume/ConsumerController.java
@@ -0,0 +1,36 @@
+package com.eureka.consume;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+public class ConsumerController {
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancerClient loadBalancerClient;
+
+    @GetMapping("/consume")
+    public String consume(){
+        ServiceInstance serviceInstance = loadBalancerClient.choose("service-hi");
+        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/cloudClient";
+        System.out.println(url);
+        return restTemplate.getForObject(url, String.class);
+    }
+
+    @GetMapping("/consumeActive")
+    public String consumeActive(){
+        System.out.println(123);
+        return "consume";
+    }
+}
