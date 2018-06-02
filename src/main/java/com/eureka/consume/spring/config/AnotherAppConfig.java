e1043ee022fbba230f5dc5d2ed5ba72837ad2ef2
dengwei <1175341729@qq.com>
2018-06-02 15:31:34 +0800

    apollo集成到配置中心

---
+++ src/main/java/com/eureka/consume/spring/config/AnotherAppConfig.java
@@ -0,0 +1,12 @@
+package com.eureka.consume.spring.config;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Jason Song(song_s@ctrip.com)
+ */
+@Configuration
+@EnableApolloConfig(value = "TEST1.apollo", order = 11)
+public class AnotherAppConfig {
+}
